2229bafb05b7479e7a4591bb58fe5025e375e563
Tichaona Zvidzayi <tzvidzie@gmail.com>
2021-12-12 22:50:00 +0200

    Update 99metrics.bash

--- 99metrics.bash
+++ 99metrics.bash
@@ -9,9 +9,9 @@
 # A bash script to allow fat clients to route external network
 
 #!/bin/bash 
-sudo ifmetric eno1 50     #connects to the public Internet (external network)
-sudo ifmetric enp2so 100   #connects the private network
-sudo echo 1 > /proc/sys/net/ipv4/ip_forward                              # turned off by default
-sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
-sudo iptables -A FORWARD -i eno1 -o enp2s0 -m state -–state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i enp2s0 -o eno1 -j ACCEPT
+ifmetric eno1 50     #connects to the public Internet (external network)
+ifmetric enp2so 100   #connects the private network
+echo 1 > /proc/sys/net/ipv4/ip_forward                              # turned off by default
+iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+iptables -A FORWARD -i eno1 -o enp2s0 -m state -–state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i enp2s0 -o eno1 -j ACCEPT
