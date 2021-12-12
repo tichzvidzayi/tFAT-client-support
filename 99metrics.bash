117e885095718f0756e47d4a1cb342d330cfcaf5
Tichaona Zvidzayi <tzvidzie@gmail.com>
2021-12-12 22:44:23 +0200

    Update 99metrics.bash

--- 99metrics.bash
+++ 99metrics.bash
@@ -1,14 +1,16 @@
-# sudo apt install ifmetric                    ## Install ifmetric 
 
-# sudo bash -c ‘echo “txt” > /usr/local/sbin/99metrics.sh‘  #creating script file
-# sudo chmod a+x /usr/local/sbin/99metrics.sh #Make it executable
-# sudo chmod 777 /usr/local/sbin/99metrics.sh #Give it root rights (saves you   .                                            to write sudo every time)
-# sudo vim /usr/local/sbin/99metrics.sh
+
+# sudo bash -c ‘echo “ ” > /usr/local/sbin/99metrics.sh‘  #creating script file
+# 
+# sudo chmod 777 /usr/local/sbin/99metrics.sh 
+# sudo vim /usr/local/sbin/99metrics.sh      #use vim or any editor of choice
+
+#############################################################################################
+# A bash script to allow fat clients to route external network
 
 #!/bin/bash 
 sudo ifmetric eno1 50     #connects to the public Internet (external network)
 sudo ifmetric enp2so 100   #connects the private network
-##****************** FAT CLIENT SUPPORT********************************
 sudo echo 1 > /proc/sys/net/ipv4/ip_forward                              # turned off by default
 sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
 sudo iptables -A FORWARD -i eno1 -o enp2s0 -m state -–state RELATED,ESTABLISHED -j ACCEPT
